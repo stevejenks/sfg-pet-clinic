003aa3b631ac16025c2bedf334921582a1ae7fc6
stevejenks <steve.jenkings@gmail.com>
2018-10-14 10:07:59 +0100

    Adding pojos, Closes #1

---
+++ pet-clinic-data/src/main/java/com/jenkings/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.jenkings.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ pet-clinic-data/src/main/java/com/jenkings/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.jenkings.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
