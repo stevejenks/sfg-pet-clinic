30d9fffe16d8b387a8f8b526f4cbb0d5e9ddc349
stevejenks <steve.jenkings@gmail.com>
2018-10-16 13:02:29 +0100

    Data model enhanced to include Visit

--- pet-clinic-data/src/main/java/com/jenkings/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/jenkings/sfgpetclinic/model/Owner.java
@@ -1,7 +1,18 @@
 package com.jenkings.sfgpetclinic.model;
 
 import javax.persistence.Entity;
+import java.util.Set;
 
 @Entity
 public class Owner extends Person {
+
+   private Set<Pet> pets;
+
+   public Set<Pet> getPets() {
+      return pets;
+   }
+
+   public void setPets(Set<Pet> pets) {
+      this.pets = pets;
+   }
 }
---
+++ pet-clinic-data/src/main/java/com/jenkings/sfgpetclinic/model/Visit.java
@@ -0,0 +1,36 @@
+package com.jenkings.sfgpetclinic.model;
+
+import javax.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
+public class Visit extends BaseEntity {
+
+   private LocalDate date;
+   private String description;
+   private Pet pet;
+
+   public LocalDate getDate() {
+      return date;
+   }
+
+   public void setDate(LocalDate date) {
+      this.date = date;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   public Pet getPet() {
+      return pet;
+   }
+
+   public void setPet(Pet pet) {
+      this.pet = pet;
+   }
+}
