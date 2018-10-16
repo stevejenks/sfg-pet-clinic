bfcf8b7adf2f02be59d9e609ed6081c69d24397d
stevejenks <steve.jenkings@gmail.com>
2018-10-16 16:02:51 +0100

    Fixed menu links

--- pet-clinic-web/src/main/java/com/jenkings/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/jenkings/sfgpetclinic/controllers/VetController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping({"/vets"})
 @Controller
 public class VetController {
 
@@ -15,7 +14,7 @@ public class VetController {
       this.vetService = vetService;
    }
 
-   @RequestMapping({"", "index", "index.html"})
+   @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
    public String listVets(Model model) {
 
       model.addAttribute("vets", vetService.findAll());
