9f0e46c5c4ff7df4826e056a22ab2d0c3fcd95b6
stevejenks <steve.jenkings@gmail.com>
2018-10-16 13:13:30 +0100

    Added speciality to Vets

--- pet-clinic-data/src/main/java/com/jenkings/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/jenkings/sfgpetclinic/model/Vet.java
@@ -1,8 +1,18 @@
 package com.jenkings.sfgpetclinic.model;
 
 import javax.persistence.Entity;
+import java.util.Set;
 
 @Entity
 public class Vet extends Person {
 
+   private Set<Speciality> specialties;
+
+   public Set<Speciality> getSpecialties() {
+      return specialties;
+   }
+
+   public void setSpecialties(Set<Speciality> specialties) {
+      this.specialties = specialties;
+   }
 }
