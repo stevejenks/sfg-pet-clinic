c803d446c71087da7983a3f74b4c85f6438994dd
stevejenks <steve.jenkings@gmail.com>
2018-10-16 09:26:37 +0100

    Closed more stuff

--- pet-clinic-web/src/main/java/com/jenkings/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/jenkings/sfgpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
    public void run(String... args) throws Exception {
 
       Owner owner1 = new Owner();
-      owner1.setId(1L);
       owner1.setFirstName("Bob");
       owner1.setLastName("Billows");
 
       ownerService.save(owner1);
 
       Owner owner2 = new Owner();
-      owner2.setId(2L);
       owner2.setFirstName("Billy");
       owner2.setLastName("Marshmallow");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
       System.out.println("Loaded Owners");
 
       Vet vet1 = new Vet();
-      vet1.setId(1L);
       vet1.setFirstName("Hannah");
       vet1.setLastName("J");
 
       vetService.save(vet1);
 
       Vet vet2 = new Vet();
-      vet2.setId(2L);
       vet2.setFirstName("Jessie");
       vet2.setLastName("J");
 
